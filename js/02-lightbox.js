6a37e4dcedd7633db7d23c576f100545a6ad64e1
Diana Forostiana <dianaforost@gmail.com>
2023-02-06 10:02:56 +0100

    Update 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function createMarkup() {
+  const itemMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-sourse="${original}" alt="${description}"></a></div>`
+    )
+    .join("");
 
+  gallery.insertAdjacentHTML("afterbegin", itemMarkup);
+}
+createMarkup();
+const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 console.log(galleryItems);
